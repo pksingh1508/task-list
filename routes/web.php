6abe6a0f578261e2bef7a2c1ea32b7ed71eab911
pksingh1508 <rajiit1212@gmail.com>
2026-01-19 20:44:22 +0530

    form and csrf commands

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
-use Illuminate\Http\Response;
+use Illuminate\Http\Request;
+use App\Models\Task;
 use Illuminate\Support\Facades\Route;
 
 // Route::get('/', function () {
@@ -13,15 +14,36 @@ Route::get('/', function() {
 
 // use all() -> to return all data from the model or see docs in database query builder in laravel
 // use latest() -> to return data ordered by created_at descending
+// we can also use clauses like -> where('completed', false) to filter data
 Route::get('/tasks', function () {
     return view('index', [
-        "tasks" => \App\Models\Task::latest()->get()
+        "tasks" => Task::latest()->get()
     ]);
 })->name('tasks.index');
 
+Route::view('/tasks/create', 'create')->name('tasks.create');
+
 Route::get('/tasks/{id}', function($id) {
-    return view('show', ['task' => \App\Models\Task::findOrFail($id)]);
+    return view('show', ['task' => Task::findOrFail($id)]);
 })->name('tasks.show');
+
+// POST Request.
+Route::post('/tasks', function(Request $request) {
+    // sanitaize and validate the data
+    $data = $request->validate([
+        'title' => 'required|max:255',
+        'description' => 'required',
+        'long_description' => 'required'
+    ]);
+
+    $task = new Task;
+    $task->title = $data['title'];
+    $task->description = $data['description'];
+    $task->long_description = $data['long_description'];
+
+    $task->save();
+    return redirect()->route('tasks.show', ['id' => $task->id]);
+})->name('tasks.store');
 
 
 // Route::get('/hello', function() {
