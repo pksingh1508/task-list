bf3ade7fd6b6761bea683fade0d8e4fab97e9768
pksingh1508 <rajiit1212@gmail.com>
2026-01-19 21:33:02 +0530

    about session, errors and splash message

--- routes/web.php
+++ routes/web.php
@@ -42,7 +42,7 @@ Route::post('/tasks', function(Request $request) {
     $task->long_description = $data['long_description'];
 
     $task->save();
-    return redirect()->route('tasks.show', ['id' => $task->id]);
+    return redirect()->route('tasks.show', ['id' => $task->id])->with('success', "Task created successfully");
 })->name('tasks.store');
 
 
