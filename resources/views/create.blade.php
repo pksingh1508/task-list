82423d90c51c06408f8c65e53b6b57f6a6b5161a
pksingh1508 <rajiit1212@gmail.com>
2026-01-20 13:42:23 +0530

    re-useable views

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -1,45 +1,5 @@
 @extends('layouts.app')
 
-@section('title', 'Add Task')
-
-@section('styles')
-    <style>
-        .error-message {
-            color: red;
-            font-size: 0.8rem;
-        }
-    </style>
-@endsection
-
 @section('content')
-    <form method="POST" action="{{ route('tasks.store') }}">
-        {{-- csrf => Cross Site Request Forgery --}}
-        @csrf
-        <div>
-            <label for="title">Title</label>
-            <input type="text" name="title" id="title" value="{{ old('title') }}"/>
-            @error('title')
-                <p class="error-message">{{ $message }}</p>
-            @enderror
-        </div>
-        <div>
-            <label for="description">Description</label>
-            <textarea name="description" id="description" rows="5">{{ old('description') }}</textarea>
-            @error('description')
-                <p class="error-message">{{ $message }}</p>
-            @enderror
-        </div>
-        <div>
-            <label for="long_description">long Description</label>
-            <textarea name="long_description" id="long_description" rows="10">{{ old('long_description') }}</textarea>
-            @error('long_description')
-                <p class="error-message">{{ $message }}</p>
-            @enderror
-        </div>
-
-        <div>
-            <button type="submit">Submit</button>
-        </div>
-    </form>
-
+    @include('form')
 @endsection
